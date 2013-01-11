0f6007acb950d6b982488538051adbbeb2a43267
Ross Jones <rossdjones@gmail.com>
2013-01-11 21:39:25 +0000

    Models for the data

--- ckanext/mo_observations/model.py
+++ ckanext/mo_observations/model.py
@@ -2,23 +2,32 @@ import re
 import uuid
 
 from sqlalchemy import Table, Column, MetaData, ForeignKey
-from sqlalchemy import types
+from sqlalchemy import types, orm
 from sqlalchemy.sql import select
-from sqlalchemy.orm import mapper, relation
+from sqlalchemy.orm import mapper, relationship
 from sqlalchemy import func
+from sqlalchemy.ext.declarative import declarative_base
 
 import ckan.model as model
 from ckan.lib.base import *
 
 log = __import__('logging').getLogger(__name__)
 
+Base = declarative_base()
 
 def make_uuid():
     return unicode(uuid.uuid4())
 
 metadata = MetaData()
 
-class TimeStep(object):
+class TimeStep(Base):
+    """
+    """
+    __tablename__ = 'mo_timestep'
+    id = Column(types.UnicodeText,
+           primary_key=True,
+           default=make_uuid)
+    when = Column(types.DateTime)
 
     def __init__(self, **kwargs):
         for k,v in kwargs.items():
@@ -30,47 +39,66 @@ class TimeStep(object):
         return q.first()
 
     def __str__(self):
-        return u"<Ruleset: %s, %s>" % (self.publisher_name, self.url_regex)
+        return u"<TimeStep: %s>" % (self.when)
 
-timestep_table = Table('mo_timesteps', metadata,
-                      Column('id', types.UnicodeText, primary_key=True,
-                             default=make_uuid),
-                      Column('when', types.DateTime),
-                )
-mapper(TimeStep, timestep_table)
 
-"""
-class Ruleset(object):
+
+class Location(Base):
+    """
+    """
+    __tablename__ = 'mo_location'
+
+    id = Column(types.UnicodeText,
+           primary_key=True,
+           default=make_uuid)
+    lat = Column(types.UnicodeText)
+    lon = Column(types.UnicodeText)
+    lat = Column(types.UnicodeText)
+    name = Column(types.UnicodeText)
+    country = Column(types.UnicodeText)
+    continent = Column(types.UnicodeText)
 
     def __init__(self, **kwargs):
         for k,v in kwargs.items():
             setattr(self, k, v)
 
     @classmethod
-    def find(cls, publisher_name, url):
-        query = "select * from lf_ruleset where publisher_name=:pub and :url ~ url_regex"
-        q = model.Session.query(Ruleset).from_statement(query).params(url=url,pub=publisher_name)
-        return q.first()
+    def get(cls, name):
+        l = model.Session.query(Location).filter(Location.name==name).first()
+        if not l:
+          l = model.Session.query(Location).filter(TimeStep.id==name).first()
+        return l
 
     def __str__(self):
-        return u"<Ruleset: %s, %s>" % (self.publisher_name, self.url_regex)
+        return u"<Location: %s>" % (self.name)
 
-ruleset_table = Table('lf_ruleset', metadata,
-                      Column('id', types.UnicodeText, primary_key=True,
-                             default=make_uuid),
-                      Column('publisher_name', types.UnicodeText),
-                      Column('url_regex', types.UnicodeText),
-                      Column('follow_link', types.UnicodeText),
-                      Column('xpath', types.UnicodeText),
-                      Column('css', types.UnicodeText),
-                      Column('link_regex', types.UnicodeText),
-                      Column('link_text_regex', types.UnicodeText),
-                      Column('allow_multiple', types.Integer),
-                )
-mapper(Ruleset, ruleset_table)
 
-"""
+class Observation(Base):
+    """
+    """
+    __tablename__ = 'mo_observation'
+
+    id = Column(types.UnicodeText,
+           primary_key=True,
+           default=make_uuid)
+    weather_type = Column( types.UnicodeText)
+    visibility = Column( types.UnicodeText)
+    temperature = Column(types.UnicodeText)
+    wind_speed = Column(types.UnicodeText)
+    pressure = Column(types.UnicodeText)
+    wind_gust = Column(types.UnicodeText)
+    wind_direction = Column(types.UnicodeText)
+    timestep_id = Column(types.UnicodeText, ForeignKey('mo_timestep.id'), nullable=False)
+    location_id = Column(types.UnicodeText, ForeignKey('mo_location.id'), nullable=False)
+    location = relationship(Location,primaryjoin="Observation.location_id==Location.id")
+    timestep = relationship(TimeStep,primaryjoin="Observation.timestep_id==TimeStep.id", backref='observations')
+
+    def __init__(self, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return u"<Observation: %s, %s>" % (self.name)
 
 def init_tables():
-    metadata.create_all(model.meta.engine)
-
+    Base.metadata.create_all(model.meta.engine)
